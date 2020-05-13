9a3c770ba23c4ceaa0aa41625d88d5bf5484ec65
Jingshu Wang <wangjingshususan@gmail.com>
2020-05-13 00:01:53 -0500

    Update function descriptions

--- R/mr_fun_general.R
+++ R/mr_fun_general.R
@@ -1,37 +1,48 @@
 #' Robust multivariate MR estimation
 #'
-#' @param b_exp A matrix of size \code{p * k} for the effect sizes of \code{p} number of independent SNPs on \code{k} risk factors
-#' @param b_out A vector of length \code{p} for the effect sizes of the \code{p} SNPs on the outcome
-#' @param se_exp A matrix of size \code{p * k} for the standard deviations of the effect sizes in \code{b_exp} 
-#' @param se_out A vector of length \code{p} for the standard deviations of the effect sizes in \code{b_out}
-#' @param tau2 The dispersion parameter, by default to be estimated from the function
-#' @param cor.mat Either NULL or a \code{k + 1} by \code{k + 1} symmetric matrix. The correlation matrix of estimated effect sizes on the \code{k} risk factors and the outcome. Default is NULL, for the identity matrix
-#' @param loss.function Loss function used, one of "l2", "huber" and "tukey"
+#' The main function of GRAPPLE to estimate causal effects of risk factors \code{beta} under a random effect model of the pleiotropic effects.
+#'
+#' @param b_exp A matrix of size \code{p * k} for the effect sizes of \code{p} number of selected independent SNPs (instruments) on \code{k} risk factors
+#' @param b_out A vector of length \code{p} for the effect sizes of the selected \code{p} SNPs on the disease (outcome)
+#' @param se_exp A matrix of size \code{p * k} for the standard deviations of each entry in \code{b_exp} 
+#' @param se_out A vector of length \code{p} for the standard deviations of each element in \code{b_out}
+#' @param tau2 The dispersion parameter. The default value is NULL, which is to be determined automatically 
+#' @param cor.mat Either NULL or a \code{k + 1} by \code{k + 1} symmetric matrix. The shared correlation matrix for \code{(b_exp[j], b_out[j])} across SNP j. Default is NULL, for the identity matrix
+#' @param loss.function Loss function used, one of "tukey", "huber" or "l2". Default is "tukey", which is robust to outlier SNPs with large pleiotropic effects
 #' @param k Tuning parameters of the loss function, for loss "l2", it is NA, for loss "huber", default is 1.345 and for loss "tukey", default is 4.685
-#' @param suppress.warning Whether suppress warning messages or not, default is FALSE
-#' @param diagnosis Run diagnosis analysis based on the residuals or not, default is FALSE
-#' @param niter Number of iterations for optimization. Default is 20
+#' @param niter Number of maximum iterations allowed for optimization. Default is 20
 #' @param tol Tolerance for convergence, default is the square root of the smallest positive floating number depending on the machine R is running on
+#' @param opt.method the optimization used, which is one of choices the R function \code{optim} accepts. Default value is "L-BFGS-B".
+#' @param diagnosis Run diagnosis analysis based on the residuals or not, default is FALSE
+#'
+#' @return A list with elements
+#' \item{beta.hat}{Point estimates of \code{beta}}
+#' \item{tau2.hat}{Point estimates of the pleiotropic effect variance \code{tau2} if the argument \code{tau2} is set to NULL}
+#' \item{beta.variance}{Estimated covariance matrix of \code{beta.hat}}
+#' \item{tau2.se}{Estimated standard deviation of \code{tau2.hat}}
+#' \item{beta.p.vaue}{A vector of p-values where the kth element is the p-value for whether \code{beta_k = 0}}
+#' \item{std.resid}{Returned if \code{diagnosis} is TRUE. A vector of standardized residuals of each SNP}
+#'
 #' @import nortest
 #' @export
 grappleRobustEst <- function(b_exp, b_out, 
                              se_exp, se_out,
                              tau2 = NULL,
                              cor.mat = NULL, 
-                             loss.function = c("l2", "huber", "tukey"), 
+                             loss.function = c("tukey", "huber", "l2"), 
                              k = switch(loss.function[1], 
                                         l2 = NA, huber = 1.345, 
                                         tukey = 4.685), 
-                             suppress.warning = FALSE, 
-                             diagnosis = FALSE,
                              niter = 20, 
                              tol = .Machine$double.eps^0.5,
-                             opt.method = "L-BFGS-B") {
-
+                             opt.method = "L-BFGS-B",
+                             diagnosis = FALSE) {
     b_exp <- as.matrix(b_exp)
     se_exp <- as.matrix(se_exp)
+	b_out <- as.vector(b_out)
+	se_out <- as.vector(se_out)
 
-    loss.function <- match.arg(loss.function, c("l2", "huber", "tukey"))
+    loss.function <- match.arg(loss.function, c("tukey", "huber", "l2"))
     if (is.null(cor.mat))
       cor.mat <- diag(rep(1, ncol(b_exp) + 1))
     rho <- switch(loss.function,
@@ -48,6 +59,7 @@ grappleRobustEst <- function(b_exp, b_out,
     c4 <- integrate(function(x) rho(x, deriv = 1) * x * dnorm(x), -Inf, Inf)$value
  #   c3 <- integrate(function(x) (rho(x, deriv = 2) * x^2 + rho(x, deriv = 1) * x)* dnorm(x), -Inf, Inf)$value
     c3 <- c4
+
 
     ## First, calculate t for each SNP
     t_fun <- function(beta, tau2) {
@@ -78,9 +90,10 @@ grappleRobustEst <- function(b_exp, b_out,
       tau2.hat <- 0
     else
       tau2.hat <- tau2
-    bound.beta <- apply(abs(b_out / b_exp), 2, function(v)quantile(v[is.finite(v)], probs = 0.95, na.rm = T)) * 2
-    bound.tau2 <- quantile(se_out^2, 0.95, na.rm = T) * 2
-  #  print(bound.tau2)
+    bound.beta <- apply(abs(b_out / b_exp), 2, function(v)quantile(v[is.finite(v)], 
+																   probs = 0.95, na.rm = T)) * 2
+   # bound.tau2 <- quantile(se_out^2, 0.95, na.rm = T) * 2
+    bound.tau2 <- median(b_out^2) * 2
     if (ncol(b_exp) == 1) {
       beta.seq <- seq(-bound.beta, bound.beta, length.out = 5000)
       beta.hat <- beta.seq[which.max(sapply(beta.seq, robust.optfun.fixtau, tau2 = 0))]
@@ -100,7 +113,6 @@ grappleRobustEst <- function(b_exp, b_out,
       #                          tol = .Machine$double.eps^0.5)$maximum 
       }
     }
-
  #   beta.hat <- beta.hat + rnorm(length(beta.hat), bound.beta * 0.05)
 
     for (iter in 1:niter) {
@@ -112,11 +124,14 @@ grappleRobustEst <- function(b_exp, b_out,
 
         if (temp < 0)
           tau2.hat <- 0
-        else
+        else {
           tau2.hat <- tryCatch(uniroot(function(tau2) sum(robust.E(beta.hat, tau2)), 
-                                       bound.tau2 * c(0, 1), extendInt = "downX", 
-                                       tol = bound.tau2 * .Machine$double.eps^0.25)$root, 
+                                       bound.tau2 * c(0, 1), 
+									   extendInt = "downX", 
+                                       tol = .Machine$double.eps^0.25)$root, 
+                                    #   tol = bound.tau2 * .Machine$double.eps^0.25)$root, 
                                error = function(e) {warning("Did not find a solution for tau2."); 0})
+		}
       }
 
    #     if (tau2.hat < 0) {
@@ -203,12 +218,12 @@ grappleRobustEst <- function(b_exp, b_out,
     if (s == 5)
       warning("Did not converge in the final step. May not find the global optimal point.")
 
-    if (is.null(tau2) && tau2.hat > bound.tau2) {
-      warning("Estimated overdispersion seems abnormaly large.")
-    }
+  #  if (is.null(tau2) && tau2.hat > bound.tau2) {
+  #    warning("Estimated overdispersion seems abnormaly large.")
+  #  }
 
  
-    if (is.null(tau2) && (tau2.hat <= min(se_out^2) / 5) && (!suppress.warning)) {
+    if (is.null(tau2) && (tau2.hat <= min(se_out^2) / 5)) {
         warning("The estimated overdispersion parameter is very small. 
                 Consider using the simple model without overdispersion.")
     }
@@ -308,6 +323,11 @@ rho.huber <- function(r, k = 1.345, deriv = 0) {
 
 #' Tukey's beweight loss function and its derivatives
 #'
+#' @param r Function value
+#' @param k Tuning parameter, default value is 4.685
+#' @param deriv The derivative function to calculate. 0 is the Tukey's loss function, 1 is for the first derivative and 2 for the second derivative function
+#'
+#' @return The value of the corresponding function at \code{r}
 #' @import stats
 #' @export
 #'
@@ -319,7 +339,9 @@ rho.tukey <- function(r, k = 4.685, deriv = 0) {
     } else if (deriv == 2) {
         t <- (r/k)^2
         ifelse(t < 1, (1 - t) * (1 - 5 * t), 0)
-    }
+    } else {
+		stop("Higher order derivatives not implemented")
+	}
 }
 
 #' L2 loss function and its derivatives
